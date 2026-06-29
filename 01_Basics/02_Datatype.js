37e501a3324bf07cc9f7c53fbe0e5999185e5af0
nitinjori <jorinitin54@gmail.com>
2026-06-29 15:49:41 +0000

    Datatypes in js

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,16 @@
+let name="Nitin"
+let age=23
+let isMarried="false"
+let city;
+
+console.log(age);
+
+console.log(typeof(name));
+console.log(typeof(city));
+
+let state=null;
+
+console.log(typeof(state));
+
+let BigInt=accountId();
+console.log(typeof(BigInt))
